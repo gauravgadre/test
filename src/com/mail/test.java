94f767c2c644c3134d7743a22853879d5a3f73dd
GauravGadre123 <ggadre56@gmail.com>
2023-12-26 18:43:52 +0530

    'Added'

--- src/com/mail/test.java
+++ src/com/mail/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		new SendInlineImagesInEmail("gauravgadre94@gmail.com", "Test Email", "Hi, This is a test email with image.",
-			"C:\\Users\\A\\Desktop\\jai.jpg");
+			"C:\\Users\\A\\Desktop");
 	}
 	
 }
